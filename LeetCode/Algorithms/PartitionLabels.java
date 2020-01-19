11cee56707060dcfd7645483ace8375349c3ec9f
Devashish Badlani <devashishbadlani@me.com>
2020-01-19 15:33:12 -0800

    partition labels

--- LeetCode/Algorithms/PartitionLabels.java
+++ LeetCode/Algorithms/PartitionLabels.java
@@ -1,8 +1,7 @@
 /* 
     https://leetcode.com/problems/partition-labels/
-    
-    A string S of lowercase letters is given. We want to partition this string into as many parts as       possible so that each letter appears in at most one part, and return a list of integers                 representing the size of these parts.
-    
+    This is similar to Jump Game problem
+    A string S of lowercase letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
     
     Input: S = "ababcbacadefegdehijhklij"
     Output: [9,7,8]
