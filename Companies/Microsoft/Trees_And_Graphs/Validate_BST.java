1d895d559558e351ff3a7b9d9fcd360488ece2ac
Devashish Badlani <devashishbadlani@me.com>
2020-01-25 21:15:17 -0800

    Tree and Graph start

---
+++ Companies/Microsoft/Trees_And_Graphs/Validate_BST.java
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/explore/interview/card/microsoft/31/trees-and-graphs/152/
+ * https://leetcode.com/problems/validate-binary-search-tree/
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ * https://leetcode.com/problems/validate-binary-search-tree/
+ * We always need our parent value for us to compare
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+}
