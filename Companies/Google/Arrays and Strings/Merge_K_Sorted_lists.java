48ef4384911510f25a3a715c818794fd9f1b2ea5
Devashish Badlani <devashishbadlani@me.com>
2020-02-09 16:57:55 -0800

    practice questions from various chapters

---
+++ Companies/Google/Arrays and Strings/Merge_K_Sorted_lists.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        /*
+        Declare a Priority Queue with comparator (a, b) -> a.val-b.val
+        Declare a dummyNode
+        Point tail to dummyNode
+        
+        for(ListNode node: lists)
+            if (node!=null)
+                queue.add(node)
+        
+        */
+        if (lists==null||lists.length==0) return null;
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>((a,b) -> a.val-b.val);
+        ListNode dummy = new ListNode(0);
+        ListNode outputHead=dummy;
+        
+        for (ListNode node:lists) {
+            if (node!=null)
+                queue.add(node);
+        }
+
+        while (!queue.isEmpty()){
+
+            ListNode priorityNode = queue.poll(); // important Node smallestNode remove
+            outputHead.next = priorityNode;
+            outputHead = outputHead.next; // move it forward to next important element
+            
+            if (outputHead.next!=null) //add the other elements in the queue
+                queue.offer(outputHead.next); // offer() or add()
+        }
+        return dummy.next;
+    }
+
+}
---
+++ Companies/Google/Design/Min_Stack.java
@@ -0,0 +1,32 @@
+/*
+https://leetcode.com/problems/min-stack/
+
+*/
+class MinStack {
+    Stack<Integer> mainStack = new Stack<Integer>();
+    Stack<Integer> minStack = new Stack<Integer>();
+    
+    public void push(int x) {
+        mainStack.push(x);
+        if (minStack.empty()) {
+            minStack.push(x);
+        } else if (minStack.peek() >= x) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        int poppedElement = mainStack.pop();
+        if (poppedElement == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return mainStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
